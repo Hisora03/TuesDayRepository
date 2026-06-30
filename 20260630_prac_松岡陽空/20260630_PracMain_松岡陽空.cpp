813bf5cdf65fc4d012c43d421d0d83132917cc45
Hisora03 <sunsky2328@gmail.com>
2026-06-30 16:26:39 +0900

    ブラックジャックもどき

---
+++ 20260630_prac_松岡陽空/20260630_PracMain_松岡陽空.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+//’è”
+const int CARD_MIN = 1;
+const int CARD_MAX = 11;
+const int BLACLJACK = 21;
+
+enum
+{
+	Yes,
+	No,
+};
+
+void CardType(int card)
+{
+	switch (card)
+	{
+	case Yes:
+		break;
+	}
+}
+
+int main(void)
+{
+
+	//’è”
+	int ply;
+	int cpu;
+	int 
+	cout << "Blacljack" << endl;
+
+
+	return 0;
+}
