b9f92365a60f8763c18f3f8df10b1b9456fb01f0
senahomura <senahomura@gmail.com>
2026-08-18 16:02:38 +0900

    ポインタ（説明あり）

--- 0818_Task/20260818_Prac2_cpp.cpp
+++ 0818_Task/20260818_Prac2_cpp.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
 
-void Damage(int* p)
+void Damage(int* p)//HP‚ÌZŠ*‚‚É-20‚É‚·‚é‚±‚Æ‚É‚æ‚Á‚ÄHP(80)100-20‚É‚µ‚Ä‚¢‚é
 {
 	//*p=-20
 	*p -= 20;
 }
-void Heal(int *num)
+void Heal(int *num)//100-20‚ÌHP‚ÌZŠ‚ÌXVBHP(100-20)+30‚É‚µ‚Ä‚¢‚éHP=110(100-20+30)
 {
 	*num += 30;
 }
 int main()
 {
-	int HP = 100;
+	int HP = 100;//HP‚P‚O‚O
 	Damage(&HP);
 	Heal(&HP);
 	cout << HP << endl;
